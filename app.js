6140a5c4420afc4290ee62ae0e0cfa8c9a19cea1
Hubenko Oksana <17zov89@gmail.com>
2021-06-09 16:03:58 +0300

    swaggwr, signup, login, logout

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+const helmet = require('helmet')
+const boolParser = require('express-query-boolean')
+const rateLimit = require('express-rate-limit')
+const swaggerUi = require('swagger-ui-express')
+const swaggerDocument = require('./swagger.json')
 
-const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/users')
 
 const app = express()
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(logger(formatsLogger))
-app.use(cors())
+app.use(helmet())
+app.get('env') !== 'test' && app.use(logger(formatsLogger))
+app.use(express.static('public'))
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  handler: (req, res, next) => {
+    return res.status(429).json({
+      status: 'error',
+      code: 429,
+      message: 'Too Many Requests',
+    })
+  },
+})
+
+app.use(limiter)
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  }),
+)
 app.use(express.json())
+app.use(boolParser())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
 
-app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' })
+app.use((_, res, __) => {
+  res.status(404).json({
+    status: 'error',
+    code: 404,
+    data: 'Not found',
+  })
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  const status = err.status || 500
+  res.status(status).json({
+    status: status === 500 ? 'fail' : 'error',
+    code: status,
+    message: err.message,
+  })
 })
 
 module.exports = app
