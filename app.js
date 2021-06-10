94e4a0a7a65bfdbc516720dd3d75c131a0f6d81b
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-06-10 14:02:22 +0300

    card start

--- app.js
+++ app.js
@@ -1,64 +1,64 @@
-const express = require('express')
-const logger = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const boolParser = require('express-query-boolean')
-const rateLimit = require('express-rate-limit')
-const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./swagger.json')
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const boolParser = require("express-query-boolean");
+const rateLimit = require("express-rate-limit");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const usersRouter = require('./routes/users')
+const usersRouter = require("./routes/users");
 
-const app = express()
+const app = express();
 
-const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(helmet())
-app.get('env') !== 'test' && app.use(logger(formatsLogger))
-app.use(express.static('public'))
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.use(helmet());
+app.get("env") !== "test" && app.use(logger(formatsLogger));
+app.use(express.static("public"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
   handler: (req, res, next) => {
     return res.status(429).json({
-      status: 'error',
+      status: "error",
       code: 429,
-      message: 'Too Many Requests',
-    })
+      message: "Too Many Requests",
+    });
   },
-})
+});
 
-app.use(limiter)
+app.use(limiter);
 app.use(
   cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
     preflightContinue: false,
     optionsSuccessStatus: 204,
-  }),
-)
-app.use(express.json())
-app.use(boolParser())
+  })
+);
+app.use(express.json());
+app.use(boolParser());
 
-app.use('/api/users', usersRouter)
+app.use("/api/users", usersRouter);
 
 app.use((_, res, __) => {
   res.status(404).json({
-    status: 'error',
+    status: "error",
     code: 404,
-    data: 'Not found',
-  })
-})
+    data: "Not found",
+  });
+});
 
 app.use((err, req, res, next) => {
-  const status = err.status || 500
+  const status = err.status || 500;
   res.status(status).json({
-    status: status === 500 ? 'fail' : 'error',
+    status: status === 500 ? "fail" : "error",
     code: status,
     message: err.message,
-  })
-})
+  });
+});
 
-module.exports = app
+module.exports = app;
